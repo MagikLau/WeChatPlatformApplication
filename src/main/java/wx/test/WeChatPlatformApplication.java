ab4d3b46cdd3a81882c8fc6fdf004dec3c7bc4e7
Lau <Lau_Itachi@163.com>
2018-07-02 20:07:42 +0800

    [VersionUpdate] Version of dependency updated to latest to fix bug from FastBootWeiXin. Update project introduction in main file.

--- src/main/java/wx/test/WeChatPlatformApplication.java
+++ src/main/java/wx/test/WeChatPlatformApplication.java
@@ -40,9 +40,9 @@ public class WeChatPlatformApplication {
         System.out.println("Text");
         return WxMessage.News.builder()
                 .addItem("基于SpringBoot框架的微信公众平台搭建",
-                        "1)\t项目搭建：使用阿里云的云服务器，系统CentOS_7.4，已完成Java应用环境搭建；\n" +
+                        "1)\t项目搭建：已转移到腾讯云进行备案，可正常运行应用\n" +
                                 "2)\t技术栈：Maven管理整体Web应用、FastBootWeChat搭建微信公众平台应用、SpringMVC管理MVC架构、网页样式使用开源样式库WeUI；\n" +
-                                "3)\t工作进度：已完成整体框架搭建、公众平台可以完成学号绑定，网页端可以登录查看学生名单，已完成绑定可以操作解除绑定，点击查看详情\n",
+                                "3)\t工作进度：微信端完成学号绑定功能，完成功能扩展以方向选择为例；网页端可查看学生名单可对已绑定微信号的学生进行解绑，可进行消息群发及分组发送；已搭建初版wiki，欢迎查看\n",
                         "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515134429744&di=4a341de585024ca5791a3e2f64e0c5e2&imgtype=0&src=http%3A%2F%2Fdesignbeats.net%2Fwp-content%2Fuploads%2F2014%2F10%2Fgithub.png",
                         "https://github.com/LauItachi/WeChatPlatformApplication")
                 .build();
@@ -75,7 +75,7 @@ public class WeChatPlatformApplication {
     @WxButton(type = WxButton.Type.VIEW,
             group = WxButton.Group.RIGHT,
             order = WxButton.Order.FIRST,
-            url = "http://magiklau.cn/wx/direction",//"https://wj.qq.com/s/1935142/9664",
+            url = "http://magiklau.cn/wx/direction",
             name = "方向选择")
     @WxAsyncMessage
     public WxMessage right02(WxRequest wxRequest, WxUser wxUser) {
